4c47e1b6af47be50a400cb72d25eec45f82723b6
Maksym <max.mixam07.max@gmail.com>
2024-06-27 11:57:01 +0300

    добавив json файл з точками

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,26 @@
-import { useEffect, useRef } from 'react';
 import Map, { Marker } from 'react-map-gl';
+import points from "../points.json";
 
 const token = "pk.eyJ1Ijoic3BoaW5jdGVyIiwiYSI6ImNseHdhNDhnYTBudzYycnF6OWlkdnllcHcifQ.95ouJZjAGy2wsXqQMOwHZg";
 
 const App = () => {
-  const markers = [
-    { longitude: 30.5234, latitude: 50.4501, description: 'Київ' },
-    { longitude: 24.0297, latitude: 49.8397, description: 'Львів' },
-    { longitude: 34.1048, latitude: 44.9521, description: 'Сімферополь' },
-    // Додайте більше точок, якщо потрібно
-  ];
-
   return (
     <div style={{width: "100vw", height: "100vh"}}>
       <Map
         mapboxAccessToken={token}
         style={{width: "100%", height: "100%"}}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapStyle="mapbox://styles/sphincter/clxx0dkc400y601pfgzvcf0xh"
+        projection="mercator"
       >
-        {markers.map((marker, index) => (
+        {points.map((marker, index) => (
           <Marker
             key={index}
             longitude={marker.longitude}
             latitude={marker.latitude}
             anchor="bottom"
+            maxZoom={10}
           >
-            <div style={{ color: 'red' }}>⬤</div>
-            {/* Ви можете використати іконку або кастомний компонент тут */}
+            <div style={{ color: '#E3B35F' }}>⬤</div>
           </Marker>
         ))}
       </Map>
